1572b9d73f1f63bc6d9560136fa598c00fc30bf3
DerickLFAndrade <derickspy15@gmail.com>
2023-01-15 01:16:19 -0300

    expressões lambda e comparator

---
+++ ProgramaçãoFuncional/src/Program.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import model.entities.Product;
+
+public class Program {
+public static void main (String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	List<Product> list = new ArrayList<>();
+	
+	list.add(new Product("TV", 900.0));
+	list.add(new Product("Notebook", 1200.0));
+	list.add(new Product("Tablet", 450.0));
+	
+	//Expressão lambda com arrowFunction
+	
+	list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+	
+	for(Product p : list) {
+	  System.out.println(p);	
+	}
+	
+	sc.close();
+}
+}
